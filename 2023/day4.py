e913f1bb666867a5b7d74a6e5486c543fa4e9b9c
danieljrichardson <danieljrichardson@clearwateranalytics.com>
2023-12-04 11:06:09 -0500

    ensure I caputure both problems

--- 2023/day4.py
+++ 2023/day4.py
@@ -6,6 +6,8 @@ class Card:
         self.winners = winners
         self.duplicates = 0
 
+    def score(self):
+        return 1 * pow(2, len(self.winners)-1) if len(self.winners) > 0 else 0
 
 scorecards=[]
 with open('day4-input.txt', 'r') as input:
@@ -13,11 +15,12 @@ with open('day4-input.txt', 'r') as input:
         data = re.split(r'[|:]', line)
         index = int(data[0][4:].strip())
         scorecards.append(Card(index, list(set(data[1].split()) & set(data[2].split()))))
+
 score = 0
 for i, card in enumerate(scorecards):
     if card.winners:
         for l in range(len(card.winners)):
             scorecards[i+1+l].duplicates += 1+card.duplicates
-    print(len(card.winners), card.duplicates, card.winners)
     score += card.duplicates + 1
+print(sum([ s.score() for s in scorecards]))
 print(score)
