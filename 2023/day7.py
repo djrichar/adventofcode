aa4bc7e7bf9f81c4094de7b1e29902480a27627d
danieljrichardson <danieljrichardson@clearwateranalytics.com>
2023-12-07 12:09:35 -0500

    cleanup

--- 2023/day7.py
+++ 2023/day7.py
@@ -1,39 +1,41 @@
-def __compute_type__(cards):
-    print("cards=", cards)
-    jokers = cards.count('J')
-    counts = [ count[0] for count in list(set([ (cards.count(c), c) for c in cards if c != 'J']))]
-    counts.sort(reverse=True)
-    if counts:
-        counts[0] += jokers
-    if( 5 in counts or jokers == 5):
-        return 7
-    if( 4 in counts):
-        return 6
-    if( 3 in counts and 2 in counts):
-        return 5
-    if( 3 in counts ):
-        return 4
-    if( 2 == counts.count(2)):
-        return 3
-    if( 2 in counts):
-        return 2
-    return 1
-
 
 class hand:
-    def __init__(self, cards, bid):
+    def __init__(self, cards, bid, **v):
         self.cards = cards
         self.bid = int(bid)
-        self.type = __compute_type__(cards)
+        self.cardcount = dict((c, cards.count(c)) for c in cards)
+        self.with_joker = v.get('joker', False)
+        self.type = self.__compute_type__()
+        
+
+    def __compute_type__(self):
+        counts = [ v for k, v in self.cardcount.items() if not (self.with_joker and k=='J')]
+        counts.sort(reverse=True)
+        if self.with_joker:
+            if not counts: counts.append(0)
+            counts[0] += self.cardcount.get("J", 0)
+        if( 5 in counts):
+            return 7
+        if( 4 in counts):
+            return 6
+        if( 3 in counts and 2 in counts):
+            return 5
+        if( 3 in counts ):
+            return 4
+        if( 2 == counts.count(2)):
+            return 3
+        if( 2 in counts):
+            return 2
+        return 1
 
     def __repr__(self) -> str:
-        return f"{self.cards}, Bid={self.bid}, Type={self.type}\n"
+        return f"{self.cards}, Bid={self.bid}, Type={self.type}, {self.cardcount}\n"
 
     def _compare(self, other):
         if self.type == other.type:
             for i,c in enumerate(self.cards):
                 def card_value(c):
-                    return int({"A": 14, "K": 13, "Q":12, "J":1, "T":10}.get(c, c))
+                    return int({"A": 14, "K": 13, "Q":12, "J": 1 if self.with_joker else 11, "T":10}.get(c, c))
                 if c != other.cards[i]:
                     return -1 if card_value(c) < card_value(other.cards[i]) else 1
             return 0
@@ -57,11 +59,14 @@ class hand:
     def __ne__(self, other):
         return self._compare(other) != 0
 
-
 hands = []
+hands2 = []
 with open('day7-input.txt', 'r') as input:
-    hands = [hand(*line.split()) for line in input.readlines()]
-
+    for line in input.readlines():
+        hands.append(hand(*line.split()))
+        hands2.append(hand(*line.split(),joker=True))
 hands.sort()
-print(hands)
 print("hands", sum([ v.bid * (index+1) for index, v in enumerate(hands)]))
+hands2.sort()
+print(hands2)
+print("hands2", sum([ v.bid * (index+1) for index, v in enumerate(hands2)]))
